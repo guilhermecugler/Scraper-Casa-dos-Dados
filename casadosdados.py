950ae039485af24f5165e7eca531ff96836e25f3
Guilhermme Cugler <guilhermecugler@gmail.com>
2023-10-05 08:21:44 -0300

    Update header

--- casadosdados.py
+++ casadosdados.py
@@ -1,3 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Description: Scraper com interface utilizando CustomTkinter do site casadosdados.com.br
+Author: Guilherme Cugler
+"""
+
 import asyncio
 import ctypes
 import json
